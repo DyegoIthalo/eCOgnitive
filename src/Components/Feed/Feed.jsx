05627dd0d64d3a832767e507c14ff950734312fc
DyegoIthalo <dyegoithalo11@hotmail.com>
2023-05-28 18:39:32 -0300

    card

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./Feed.module.css";
 
-const Feed = () => {
-  return <div>Feed</div>;
-};
-
-export default Feed;
+export default function Feed() {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardBody}>
+        <div className={styles.cardHeader}>
+          <h5 className={styles.cardTitle}>Coletor</h5>
+        </div>
+      </div>
+      <ul className={styles.cardList}>
+        <li>Nome: Coletor 1</li>
+        <li>Número: 2121478</li>
+        <li>Tipo: Monóxido de Carbono (CO)</li>
+      </ul>
+      <div className={styles.buttonContainer}>
+        <button className={styles.deleteButton}>Editar</button>
+        <button className={styles.deleteButton}>Deletar</button>
+      </div>
+    </div>
+  );
+}
